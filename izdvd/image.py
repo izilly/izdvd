88d75022f00de92fae0f77c9f6bbf63cd54c189e
Will Adams <willadams+dev@gmail.com>
2013-06-19 21:26:49 -0400

    initial commit

---
+++ izdvd/image.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2013 William Adams
+#  Distributed under the terms of the Modified BSD License.
+#  The full license is in the file LICENSE, distributed with this software.
+#
+
+import subprocess
+import os.path
+import shutil
+import tempfile
+import math
+
+class Error(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class Img (object):
+    def __init__(self, path):
+        print(type(self))
+        self.uid = str(id(self))
+        self.versions = []
+        self.update_versions(path)
+        self.orig_name = self.name
+        self.orig_ext = self.ext
+        self.tmpdir = os.path.join(tempfile.gettempdir(), __name__, 
+                                   self.orig_name+self.uid)
+        if not os.path.exists(self.tmpdir):
+            os.makedirs(self.tmpdir)
+        self.width = self.get_width()
+        self.height = self.get_height()
+        self.orig_width = self.width
+        self.orig_height = self.height
+        self.x_offset = 0
+        self.y_offset = 0
+        self.ar = self.width / self.height
+        self.orig_ar = self.ar
+    
+    def get_width(self):
+        w = subprocess.check_output(['identify', '-format', '%w', self.path], 
+                                    universal_newlines=True)
+        w = int(w.strip())
+        return w
+
+    def get_height(self):
+        h = subprocess.check_output(['identify', '-format', '%h', self.path], 
+                                    universal_newlines=True)
+        h = int(h.strip())
+        return h
+    
+    def update_dims(self):
+        self.width = self.get_width()
+        self.height = self.get_height()
+        return (self.width, self.height)
+    
+    def update_versions(self, new_version):
+        self.path = new_version
+        self.versions.append(self.path)
+        self.basename = os.path.basename(self.path)
+        self.name, self.ext = os.path.splitext(self.basename)
+    
+    def clear_offsets(self):
+        '''Resets any saved offset information.  Any future operations which 
+        record offset information will be relative to this location, rather
+        than the original image.
+        '''
+        self.x_offset = 0
+        self.y_offset = 0
+    
+    def write(self, overwrite=False, out_dir=None, 
+              out_basename=None, suffix_name=True, backup=True):
+        if overwrite:
+            if backup:
+                bak = shutil.move(self.versions[0], self.versions[0]+'.bak')
+            written = shutil.copy(self.path, self.version[0])
+            self.update_versions(written)
+            return written
+        if out_dir is None:
+            out_dir = self.tmpdir
+        if out_basename is None:
+            if suffix_name is True:
+                out_basename = self.basename
+            else:
+                out_basename = self.orig_name+self.ext
+        out_path = os.path.join(out_dir, out_basename)
+        if os.path.exists(out_path):
+            if os.path.samefile(out_path, self.path):
+                return out_path
+            else:
+                bak = shutil.move(out_path, out_path+'.bak')
+        written = shutil.copy(self.path, out_path)
+        self.update_versions(written)
+        return written
+    
+    def transcode(self, out_file=None, out_fmt='png'):
+        if out_file is None:
+            out_file = os.path.join(self.tmpdir, self.name+'.'+out_fmt)
+        o = subprocess.check_output(['convert', self.path, out_file], 
+                                    universal_newlines=True)
+        self.update_versions(out_file)
+        return out_file
+    
+    def resize(self, width, height, ignore_aspect=False, out_file=None, 
+               out_fmt='png'):
+        if out_file is None:
+            out_file = os.path.join(self.tmpdir, 
+                        self.name+'-resize-{}x{}.'.format(width, height)+out_fmt)
+        flags=''
+        if ignore_aspect:
+            flags='!'
+        size = '{}x{}{}'.format(width, height, flags)
+        o = subprocess.check_output(['convert', self.path, '-resize', size, 
+                                     out_file], universal_newlines=True)
+        self.update_versions(out_file)
+        return out_file
+    
+    def pad(self, color='none', north=0, south=0, east=0, west=0, 
+            out_file=None, out_fmt='png'):
+        if out_file is None:
+            out_file = os.path.join(self.tmpdir, self.name+'-padded.'+out_fmt)
+        splice_opts = []
+        for k,v in {'north':north, 'south':south}.items():
+            if v > 0:
+                splice_opts.extend(['-gravity', k, '-splice', '0x{}'.format(v)])
+        for k,v in {'east':east, 'west':west}.items():
+            if v > 0:
+                splice_opts.extend(['-gravity', k, '-splice', '{}x0'.format(v)])
+        print(splice_opts)
+        cmd = ['convert', self.path, '-background', color]
+        cmd.extend(splice_opts)
+        cmd.append(out_file)
+        o = subprocess.check_output(cmd, universal_newlines=True)
+        self.update_versions(out_file)
+
+    def pad_centered(self, color='none', pad_x=0, pad_y=0, out_file=None, 
+                     out_fmt='png'):
+        if out_file is None:
+            out_file = os.path.join(self.tmpdir, self.name+'-padded.'+out_fmt)
+        new_w = self.get_width() + pad_x
+        new_h = self.get_height() + pad_y
+        self.pad_to(color=color, new_w=new_w, new_h=new_h, out_file=out_file,
+                    out_fmt=out_fmt)
+
+    def pad_to(self, color='none', new_w=None, new_h=None, out_file=None, 
+                     out_fmt='png'):
+        if out_file is None:
+            out_file = os.path.join(self.tmpdir, self.name+'-padded.'+out_fmt)
+        extent_dims = '{}x{}'.format(new_w, new_h)
+        o = subprocess.check_output(['convert', self.path, '-gravity', 'center',
+                                     '-background', color, '-extent', 
+                                     extent_dims, out_file], 
+                                    universal_newlines=True)
+        self.update_versions(out_file)
+    
+    def pad_to_ar(self, ar, color='none', out_file=None, out_fmt='png'):
+        if out_file is None:
+            out_file = os.path.join(self.tmpdir, self.name+'-pad_ar.'+out_fmt)
+        if ar > self.ar:
+            new_w = math.ceil(self.height * ar)
+            new_h = self.height
+        elif ar < self.ar:
+            new_w = self.width
+            new_h = math.ceil(self.width / ar)
+        else:
+            return
+        self.pad_to(color, new_w, new_h, out_file, out_fmt)
+    
+    def border(self, color, geometry, out_file=None, out_fmt='png'):
+        if out_file is None:
+            out_file = os.path.join(self.tmpdir, self.name+'-border.'+out_fmt)
+        before_w, before_h = self.update_dims()
+        o = subprocess.check_output(['convert', self.path, '-compose', 'Copy', 
+                                     '-bordercolor', color, '-border', 
+                                     str(geometry), out_file], 
+                                    universal_newlines=True)
+        self.update_versions(out_file)
+        new_w, new_h = self.update_dims()
+        new_x_offset = (new_w-before_w)/2
+        new_y_offset = (new_h-before_h)/2
+        self.x_offset += new_x_offset
+        self.y_offset += new_y_offset
+        return out_file
+    
+    def drop_shadow(self, color='black', opacity=80, sigma=3, 
+                    x_offset=5, y_offset=5, out_file=None, out_fmt='png'):
+        if out_file is None:
+            out_file = os.path.join(self.tmpdir, self.name+'-shadow.'+out_fmt)
+        shadow_opts = '{}x{}{:+}{:+}'.format(opacity, sigma, x_offset, y_offset)
+        o = subprocess.check_output(['convert', self.path, '(', '+clone', 
+                                     '-background', color, '-shadow', 
+                                     shadow_opts, ')', '+swap', '-background', 
+                                     'none', '-layers', 'merge', '+repage', 
+                                     out_file], 
+                                    universal_newlines=True)
+        # calculate new offset (cannot be less than 0)
+        canvas_padding = sigma*2
+        new_x_offset = canvas_padding - x_offset
+        if new_x_offset < 0:
+            new_x_offset = 0
+        new_y_offset = canvas_padding - y_offset
+        if new_y_offset < 0:
+            new_y_offset = 0
+        # add new offset to existing offset
+        self.x_offset += new_x_offset
+        self.y_offset += new_y_offset
+        self.update_versions(out_file)
+        return out_file
+    
+    def overlay_onto(self, img, x_offset, y_offset, layers_method):
+        '''Overlay self onto img. Does not modify self or create a new version.
+        Returns:  new composed image.
+        '''
+        pass
+    
+    def new_layer(self, img, x_offset, y_offset, use_orig_origin=False, 
+                  layers_method='merge', out_file=None, out_fmt='png'):
+        '''Overlay img onto self.  Modifies self and adds a new version with
+        img composed onto self (and flattened).  Does not modify img.
+        Returns:  self (with new layer flattened onto canvas)
+        '''
+        if out_file is None:
+            out_file = os.path.join(self.tmpdir, 
+                                    self.name+'-new_layer.'+out_fmt)
+        if type(img) == type(self):
+            if use_orig_origin:
+                x_offset -= img.x_offset
+                y_offset -= img.y_offset
+            img = img.path
+        offsets = '{:+}{:+}'.format(x_offset, y_offset)
+        o = subprocess.check_output(['convert', self.path, '-background', 'none', 
+                                     '-page', offsets, img, 
+                                     '-layers', layers_method, '+repage', 
+                                     out_file], 
+                                    universal_newlines=True)
+        if x_offset < 0:
+            self.x_offset += abs(x_offset)
+        if y_offset < 0:
+            self.y_offset += abs(y_offset)
+        self.update_versions(out_file)
+        return out_file
+    
+    def new_canvas(self, color='none', out_file=None, out_fmt='png'):
+        if out_file is None:
+            out_file = os.path.join(self.tmpdir, self.name+'-canvas.'+out_fmt)
+        o = subprocess.check_output(['convert', self.path, '-background', 
+                                     color, '-compose', 'Dst', '-flatten', 
+                                     out_file], 
+                                    universal_newlines=True)
+        return Img(out_file)
+
---
+++ izdvd/menu.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2013 William Adams
+#  Distributed under the terms of the Modified BSD License.
+#  The full license is in the file LICENSE, distributed with this software.
+#
+
+from izdvd.image import Img
+import subprocess
+import math
+from collections import Counter
+import argparse
+
+
+class BG (object):
+    def __init__(self, bg_img, button_imgs, outer_padding=30, inner_padding=30,
+                 display_ar=None):
+        self.bg_img = Img(bg_img)
+        self.button_imgs = [Img(i) for i in button_imgs]
+        self.outer_padding = outer_padding
+        self.inner_padding = inner_padding
+        self.width = self.bg_img.width
+        self.height = self.bg_img.height
+        self.usable_w = self.width - self.outer_padding
+        self.usable_h = self.height - self.outer_padding
+        self.storage_ar = self.width / self.height
+        if display_ar is None:
+            self.display_ar = self.storage_ar
+        else:
+            self.display_ar = display_ar
+        self.multiplier = self.storage_ar / self.display_ar
+        self.cell_ar = self.calc_cell_ar()
+        #~ self.pad_buttons_ar()
+        self.grid = self.get_grid_size()
+        self.cols = self.grid['cols']
+        self.rows = self.grid['rows']
+        self.cell_w = self.grid['cell_w']
+        self.cell_h = self.grid['cell_h']
+        self.cells = len(self.button_imgs)
+        self.resize_buttons()
+        self.prepare_buttons()
+        self.cell_locations = self.get_cell_locations()
+        self.overlay_buttons()
+        dd = 1
+    
+    def calc_cell_ar(self):
+        ars = Counter([i.ar for i in self.button_imgs])
+        base_ar = ars.most_common()[0][0] * self.multiplier
+        return base_ar
+    
+    #~ def calc_cell_ar(self):
+        #~ button_widths = sorted([i.width for i in self.button_imgs])
+        #~ button_heights = sorted([i.height for i in self.button_imgs])
+        #~ widest = button_widths[-1]
+        #~ tallest = button_heights[-1]
+        #~ cell_ar = (widest/tallest)*self.multiplier
+        #~ return cell_ar
+    
+    def pad_buttons_ar(self):
+        for i in self.button_imgs:
+            i.pad_to_ar(self.cell_ar)
+        
+    def get_grid_size(self):
+        '''
+        bg_ar / cell_ar = cols / rows
+                          (grid_ratio)
+        buttons = cols * rows
+        
+        buttons = rows(gr*rows)
+        buttons = gr * rows^2
+        buttons / gr = rows^2
+        sqrt(buttons / gr) = rows
+        
+        buttons = cols * (cols/gr)
+        buttons = cols^2/gr
+        buttons*gr = cols^2
+        sqrt(buttons*gr) = cols
+        '''
+        bg_w = self.width - self.outer_padding
+        bg_h = self.height - self.outer_padding
+        bg_ar = bg_w / bg_h
+        grid_ratio = bg_ar / self.cell_ar
+        buttons = len(self.button_imgs)
+        rows = math.sqrt(buttons / grid_ratio)
+        cols = math.sqrt(buttons * grid_ratio)
+        fcols = math.floor(cols)
+        frows = math.floor(rows)
+        max_area = 0
+        min_empty = 9999
+        max_methods = []
+        tied_methods = []
+        results = []
+        methods = [{'name': 'DD', 'rounding': (0,0)}, 
+                   {'name': 'DU', 'rounding': (0,1)},
+                   {'name': 'UD', 'rounding': (1,0)},
+                   {'name': 'UU', 'rounding': (1,1)}]
+        for i in methods:
+            mcols = fcols+i['rounding'][0]
+            mrows = frows+i['rounding'][1]
+            empty = mcols*mrows - buttons
+            i['empty'] = empty
+            if empty >= 0:
+                area, bw, bh = self.get_cell_size(mcols, mrows, self.cell_ar,
+                                                  self.width, self.height)
+                i['cols'] = mcols
+                i['rows'] = mrows
+                i['area'] = area
+                i['empty'] = empty
+                i['cell_w'] = bw
+                i['cell_h'] = bh
+        sufficient = [i for i in methods if i['empty'] > -1]
+        areas = sorted([i['area'] for i in sufficient])
+        max_area = areas[-1]
+        max_methods = [i for i in sufficient if i['area'] == max_area]
+        mm_empty = sorted([i['empty'] for i in max_methods])
+        least_empty = mm_empty[0]
+        best_methods = [i for i in max_methods if i['empty'] == least_empty]
+        tied_methods = [i for i in max_methods if i not in best_methods]
+        other_methods = [i for i in sufficient if i not in max_methods]
+        return best_methods[0]
+        
+        print('Best:')
+        for i in best_methods:
+            print('  {}: {}x{}  {:.0f}x{:.0f}  ({} empty)'.format(i['name'], i['cols'], 
+                                                   i['rows'], i['bw'], i['bh'],
+                                                    i['empty'] ))
+        print('')
+        if tied_methods:
+            print('Tied:')
+            for i in tied_methods:
+                print('  {}: {}x{}  {:.0f}x{:.0f}  ({} empty)'.format(i['name'], i['cols'], 
+                                                       i['rows'], i['bw'], i['bh'],
+                                                        i['empty'] ))
+            print('')
+        if other_methods:
+            print('Other:')
+            for i in other_methods:
+                print('  {}: {}x{}  {:.0f}x{:.0f}  ({} empty)'.format(i['name'], i['cols'], 
+                                                       i['rows'], i['bw'], i['bh'],
+                                                        i['empty'] ))
+            print('')
+    
+    def get_cell_size(self, cols, rows, cell_ar, bg_w, bg_h):
+        outer_padding = self.outer_padding * 2
+        inner_padding_w = self.inner_padding * (cols - 1)
+        inner_padding_h = self.inner_padding * (rows - 1)
+        padding_w = outer_padding + inner_padding_w
+        padding_h = outer_padding + inner_padding_h
+        padded_w = bg_w - padding_w
+        padded_h = bg_h - padding_h
+        col_w = padded_w / cols
+        row_h = padded_h / rows
+        cell_w = col_w
+        cell_h = cell_w/cell_ar
+        if cell_h > row_h:
+            cell_h = row_h
+            cell_w = cell_h*cell_ar
+        # return area, cell_width, cell_height
+        return cell_w*cell_h, math.floor(cell_w), math.floor(cell_h)
+    
+    def prepare_buttons(self):
+        for i in self.button_imgs:
+            #~ i.resize(self.cell_w, self.cell_h)
+            hl = i.new_canvas()
+            hl.border('green', 5)
+            i.highlight = hl
+            sl = i.new_canvas()
+            sl.border('red', 5)
+            i.select = sl
+            i.border('white', 5)
+            i.drop_shadow()
+    
+    def resize_buttons(self):
+        for i in self.button_imgs:
+            i.storage_ar = i.ar * self.multiplier
+            if i.storage_ar > self.cell_ar:
+                w = self.cell_w
+                h = math.floor(self.cell_w / self.cell_ar)
+                i.x_padding = 0
+                i.y_padding = math.floor((self.cell_h - h) / 2)
+            elif i.storage_ar < self.cell_ar:
+                w = math.floor(self.cell_h * self.cell_ar)
+                h = self.cell_h
+                i.x_padding = math.floor((self.cell_w - w) / 2)
+                i.y_padding = 0
+            else:
+                w = self.cell_w
+                h = self.cell_h
+                i.x_padding = 0
+                i.y_padding = 0
+            i.resize(w, h, True)
+    
+    def get_cell_locations(self):
+        cells = self.cells
+        cols = self.cols
+        rows = self.rows
+        cell_w = self.cell_w
+        cell_h = self.cell_h
+        bg_w = self.width
+        bg_h = self.height
+        total_cells = list(range(cells))
+        cells = []
+        padding_y = math.floor((bg_h - cell_h*rows) / (rows + 1))
+        padded_y = cell_h + padding_y
+        for r in range(rows):
+            row_cells = total_cells[len(cells):len(cells)+cols]
+            len_cells = len(row_cells)
+            padding_x = math.floor((bg_w - cell_w*len_cells) / (len_cells + 1))
+            padded_x = cell_w + padding_x
+            for rc in range(len_cells):
+                x0 = padding_x + (rc*padded_x)
+                x1 = x0 + cell_w
+                y0 = padding_y + (r*padded_y)
+                y1 = y0 + cell_h
+                c = {'x0': x0, 'y0': y0, 'x1': x1, 'y1': y1}
+                cells.append(c)
+        return cells
+    
+    def overlay_buttons(self):
+        hl = self.bg_img.new_canvas()
+        sl = self.bg_img.new_canvas()
+        for i,cell in enumerate(self.cell_locations):
+            b = self.button_imgs[i]
+            x = cell['x0'] + b.x_padding
+            y = cell['y0'] + b.y_padding
+            self.bg_img.new_layer(b, x, y, True)
+            hl.new_layer(b.highlight, x, y, True)
+            sl.new_layer(b.select, x, y, True)
+        self.highlight_img = hl
+        self.select_img = sl
+    
+
+
