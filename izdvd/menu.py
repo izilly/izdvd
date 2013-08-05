02924f295b49d9afefc0264b2cf6ae767b0f58d2
Will Adams <willadams+dev@gmail.com>
2013-08-05 07:21:08 -0400

    support dvd creation (dvdauthor)

--- izdvd/menu.py
+++ izdvd/menu.py
@@ -30,6 +30,8 @@ class BG (object):
     def __init__(self, bg_img, button_imgs, 
                  button_labels=None, 
                  out_dir=None, out_name=None,
+                 border_px=5, border_color='white', 
+                 highlight_color='#57fb57', select_color='red',
                  label_line_height=0, label_lines=2, 
                  label_padding=5, outer_padding=30, inner_padding=30, 
                  width=None, height=None, display_ar=None):
@@ -37,6 +39,10 @@ class BG (object):
         self.button_imgs = [Img(i) for i in button_imgs]
         self.out_dir = out_dir
         self.out_name = out_name
+        self.border_px = border_px
+        self.border_color = border_color
+        self.highlight_color = highlight_color
+        self.select_color = select_color
         self.setup_out_dir()
         self.button_labels = button_labels
         self.label_line_height = label_line_height
@@ -272,12 +278,12 @@ class BG (object):
         for i in self.button_imgs:
             #~ i.resize(self.cell_w, self.cell_h)
             hl = i.new_canvas()
-            hl.border('green', 5)
+            hl.border(self.highlight_color, self.border_px)
             i.highlight = hl
             sl = i.new_canvas()
-            sl.border('red', 5)
+            sl.border(self.select_color, self.border_px)
             i.select = sl
-            i.border('white', 5)
+            i.border(self.border_color, self.border_px)
             #~ i.append([self.label_bg], padding=self.label_padding)
             #~ i.drop_shadow()
     
@@ -503,6 +509,7 @@ class DVDMenu (object):
         o = subprocess.check_output(cmd, universal_newlines=True)
     
     def multiplex_buttons(self):
+        self.buttons = []
         subpictures = etree.Element('subpictures')
         stream = etree.SubElement(subpictures, 'stream')
         spu = etree.SubElement(stream, 'spu')
@@ -510,12 +517,22 @@ class DVDMenu (object):
         spu.set('highlight', self.path_hl_img)
         spu.set('select', self.path_sl_img)
         for n,i in enumerate(self.bg.cell_locations):
+            b_name = 'b{}'.format(n)
+            self.buttons.append(b_name)
             b = etree.SubElement(spu, 'button')
-            b.set('name', 'b{}'.format(n))
-            b.set('x0', str(i['x0']))
-            b.set('x1', str(i['x1']))
-            b.set('y0', str(i['y0']))
-            b.set('y1', str(i['y1']))
+            b.set('name', b_name)
+            x0 = i['x0'] - self.bg.border_px
+            x1 = i['x1'] + self.bg.border_px
+            y0 = i['y0'] - self.bg.border_px
+            y1 = i['y1'] + self.bg.border_px
+            if y0 % 2:
+                y0 -= 1
+            if y1 % 2:
+                y1 += 1
+            b.set('x0', str(x0))
+            b.set('x1', str(x1))
+            b.set('y0', str(y0))
+            b.set('y1', str(y1))
         tree = etree.ElementTree(subpictures)
         tree.write(self.path_menu_xml, encoding='UTF-8', pretty_print=True)
         with open(self.path_menu_mpg, 'w') as f:
@@ -527,37 +544,92 @@ class DVDMenu (object):
             out, err = p2.communicate()
 
 
-class DVD (BG):
-    def __init__(self, bg_img, button_imgs, 
-                 button_labels=None, 
-                 out_dir=None, out_name=None,
-                 label_line_height=0, label_lines=2, 
-                 label_padding=5, outer_padding=30, inner_padding=30, 
-                 dvd_format='NTSC', dvd_menu_ar=4/3, dvd_menu_audio=None):
-        pass
-
-    #~ def create_dvd_xml(self):
-        #~ if self.dvd_format == 'PAL':
-            #~ fmt = 'pal'
-        #~ else:
-            #~ fmt = 'ntsc'
-        #~ if self.dvd_menu_ar == 16/9:
-            #~ menu_aspect = '16:9'
-        #~ else:
-            #~ menu_aspect = '4:3'
-        #~ 
-        #~ dvdauthor = etree.Element('dvdauthor')
-        #~ vmgm = etree.SubElement(dvdauthor, 'vmgm')
-        #~ fpc = etree.SubElement(vmgm, 'fpc')
-        #~ fpc.text = 'jump titleset 1 menu;'
-        #~ titleset = etree.SubElement(dvdauthor, 'titleset')
-        #~ menus = etree.SubElement(titleset, 'menus')
-        #~ menus_vid = etree.SubElement(menus, 'video', format=fmt, 
-                                     #~ aspect=menu_aspect)
-        #~ menus_pgc = etree.SubElement(menus, 'pgc')
-        #~ titles = etree.SubElement(titleset, 'titles')
-        #~ titles_vid = etree.SubElement(menus, 'video', format=fmt)
-        #~ titles_pgc = etree.SubElement(menus, 'pgc')
-        #~ titles_audio = etree.SubElement(menus, 'audio')
+class DVD (object):
+    def __init__(self, vid_files, menu=None, 
+                 out_dir=None, out_name=None, dvd_dir=None,
+                 dvd_format='NTSC', dvd_ar=16/9):
+        self.vid_files = vid_files
+        self.menu = menu
+        self.out_dir = out_dir
+        self.out_name = out_name
+        self.dvd_dir = dvd_dir
+        self.dvd_format = dvd_format
+        self.dvd_ar = dvd_ar
+        self.setup_out_dir()
+        self.create_dvd_xml()
+        self.author_dvd()
+    
+    def setup_out_dir(self):
+        # output directory
+        if self.menu:
+            out_dir = self.menu.out_dir
+            out_name = self.menu.out_name
+            self.path_menu_mpg = self.menu.path_menu_mpg
+        else:
+            if self.out_dir is None:
+                out_dir = os.getcwd()
+            else:
+                if not os.path.exists(out_dir):
+                    os.makedirs(out_dir)
+            # output name
+            if self.out_name is None:
+                out_time = datetime.now().strftime('%Y.%m.%d-%H%M%S')
+                out_name = 'DVD_menu_{}'.format(out_time)
+        self.out_path = os.path.join(out_dir, out_name)
+        self.out_dir = out_dir
+        self.out_name = out_name
+        self.path_dvd_xml = '{}_dvd.xml'.format(self.out_path)   # dvd.xml
+        if self.dvd_dir is None:
+            self.dvd_dir = os.path.join(self.out_dir, 'DVD')
+        else:
+            if not os.path.exists(self.dvd_dir):
+                os.makedirs(self.dvd_dir)
+    
+    def create_dvd_xml(self):
+        if self.dvd_format == 'PAL':
+            fmt = 'pal'
+        else:
+            fmt = 'ntsc'
+        if self.dvd_ar == 16/9:
+            dvd_ar = '16:9'
+        else:
+            dvd_ar = '4:3'
+        if self.menu.dvd_menu_ar == 16/9:
+            menu_ar = '16:9'
+        else:
+            menu_ar = '4:3'
+        
+        dvdauthor = etree.Element('dvdauthor')
+        vmgm = etree.SubElement(dvdauthor, 'vmgm')
+        fpc = etree.SubElement(vmgm, 'fpc')
+        fpc.text = 'jump titleset 1 menu;'
+        titleset = etree.SubElement(dvdauthor, 'titleset')
+        if self.menu:
+            menus = etree.SubElement(titleset, 'menus')
+            menus_vid = etree.SubElement(menus, 'video', format=fmt, 
+                                         aspect=menu_ar)
+            menus_pgc = etree.SubElement(menus, 'pgc')
+            for n,i in enumerate(self.menu.buttons):
+                b = etree.SubElement(menus_pgc, 'button', name=i)
+                b.text = 'jump title 1 chapter {};'.format(n+1)
+            menus_vob = etree.SubElement(menus_pgc, 'vob', 
+                                         file=self.menu.path_menu_mpg)
+            menus_post = etree.SubElement(menus_pgc, 'post')
+            menus_post.text = 'jump cell 1;'
+        titles = etree.SubElement(titleset, 'titles')
+        titles_vid = etree.SubElement(titles, 'video', format=fmt, aspect=dvd_ar)
+        titles_pgc = etree.SubElement(titles, 'pgc')
+        for i in self.vid_files:
+            v = etree.SubElement(titles_pgc, 'vob', file=i)
+        titles_post = etree.SubElement(titles_pgc, 'post')
+        titles_post.text = 'call menu;'
+        tree = etree.ElementTree(dvdauthor)
+        tree.write(self.path_dvd_xml, encoding='UTF-8', pretty_print=True)
+    
+    def author_dvd(self):
+        e = dict(os.environ)
+        e['VIDEO_FORMAT'] = 'NTSC'
+        cmd = ['dvdauthor', '-x', self.path_dvd_xml, '-o', self.dvd_dir]
+        o = subprocess.check_output(cmd, env=e, universal_newlines=True)
 
 
