678f3d7a3e86c32ff164ec1110b57783b5bc78b3
Will Adams <willadams+dev@gmail.com>
2013-09-12 20:37:20 -0400

    add setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2013 William Adams
+#  Distributed under the terms of the Modified BSD License.
+#  The full license is in the file LICENSE, distributed with this software.
+
+from distutils.core import setup
+
+setup(
+    name='izdvd',
+    version='0.3',
+    packages=['izdvd',],
+    name='izdvd',
+    version='0.1.0',
+    author='William Adams',
+    author_email='willadams+dev@gmail.com',
+    license='LICENSE',
+)
